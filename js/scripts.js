56c46136b220d08af0409bafce1de84babc26f51
Nancy El-Sharkawi <51517316+nancyelsh@users.noreply.github.com>
2020-06-26 08:45:03 +0300

    عدل على الأجوبة

--- js/scripts.js
+++ js/scripts.js
@@ -18,12 +18,28 @@ console.log(5 * myAge);
 let myHobbies = ["الرسم", "الرماية", "الجري", "السباحة"];
 
 // 2
+myHobbies[4] = 26;
+myHobbies.push(26);
+console.log(myHobbies.length);
+
+// 3
+myHobbies.pop();
+console.log(myHobbies);
+
+// 4
 let student = {
   track: "برمجة المواقع",
   language: "JavaScript 🔥",
 };
 
-// 3
+// 5
+// student["الأساتذة المساعدين"] = ["Nancy", "Moudhi", "Rahaf", "Hussain"];
+student["الأساتذة المساعدين"] = ["نانسي", "موضي", "رهف", "حسين"];
+
+// 6
 console.log(student.track);
+
+// 7
+console.log(student["الأساتذة المساعدين"][0]);
 
 // console results => console.png
