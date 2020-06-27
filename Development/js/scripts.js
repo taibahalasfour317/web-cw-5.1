f53f1cbf7c395fae27f063c8b46ada264ab10a49
طيبة جمال العصفور -317 <taibahalasfour224@gmail.com>
2020-06-27 19:56:08 +0300

    done!

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,34 @@
 // السؤال الأول
 // 1
-
+const name=("taibah")
 // 2
-
+let counter = 18 ;
 // 3 and 4
-
+console.log("انا"+ name + "وعمري" + " " + counter )
+ 
 // 5 and 6
-
+console.log (counter * 5)
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+const hoppy =[ "drawing","runing", " hopping","18"];
 // 2
-
+hoppy.push (counter)
 // 3
 
 // 4
-
+hoppy.pop()
 // 5
-
+let web ={
+name:"web",
+coding: JS ,
+TA:["moudhi","nansi","hussain"]
+}
 // 6
-
+console.log (web.name)
 // 7
-
+console.log(web.TA[0])
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
